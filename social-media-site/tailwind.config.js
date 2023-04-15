0a56f6f944f65b83007c2c412f5eb5b64ccec476
Soumil <soumil009@outlook.com>
2023-04-15 16:29:53 +0100

    configured tailwind

--- social-media-site/tailwind.config.js
+++ social-media-site/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: [],
+  content: ['index.html', 'src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
