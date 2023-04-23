89fabb461cf66ae632e2cb696f340673b3051d8a
Soumil <soumil009@outlook.com>
2023-04-23 19:13:10 +0100

    CORRECTED URL

--- social-media-site/src/views/CreateAccount.vue
+++ social-media-site/src/views/CreateAccount.vue
@@ -29,7 +29,7 @@ export default {
     createAccount() {
       const userName: String = this.userName;
       const password: String = this.password;
-      axios.post('http://localhost:3000/create-user', {
+      axios.post(`${import.meta.env.VITE_BACKEND_SERVER}create-user`, {
         userName: userName,
         password: password,
         accessCode: this.accessCode
